90ca5de6ca55c07acdb3288d6ae3d1e5ceb3f707
Nesrine <82056366+Nesrine1333@users.noreply.github.com>
2023-12-19 15:19:34 +0100

    Update Bl.controller.ts

--- src/Bl/Bl.controller.ts
+++ src/Bl/Bl.controller.ts
@@ -562,7 +562,7 @@ export class BlController {
     ): Promise<Bl[]> {
       const options: IPaginationOptions = {
         page,
-        limit: 10,
+        limit,
         route: `${userId}/${dest}/getAllBlByDest`,
       };
 
